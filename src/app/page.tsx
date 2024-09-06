eb3e6f5fcc36399a224aa27009f2499fd1ac92cf
Zandre Broodryk <zandre@pakwe.co.za>
2024-09-06 08:08:12 +0200

    fix responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ import Socials from "@/components/molecules/socials";
 export default function Home() {
   return (
     <main className="flex flex-col bg-cozy bg-cover bg-fixed bg-center">
-      <div className="flex min-h-screen flex-col items-center justify-between p-4 pt-16 before:absolute before:top-0 before:h-full before:w-full before:backdrop-blur-xs md:p-24">
+      <div className="flex min-h-screen flex-col items-center justify-between p-4 pt-16 before:absolute before:top-0 before:h-full before:w-full before:backdrop-blur-xs lg:p-24">
         <div className="grid w-fit gap-4 lg:grid-cols-2">
           <AboutMe />
           <Socials />
           <MySkills />
-          <div className="md:-mt-28">
+          <div className="lg:-mt-28">
             <CvSummary />
           </div>
         </div>
