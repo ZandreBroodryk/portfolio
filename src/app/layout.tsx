69da1ac192a847351057ef9d3e8a1b90ee17a167
Zandré Broodryk <z.broodryk@gmail.com>
2025-06-18 19:06:00 +0200

    tweak styling and scrolling on smaller devices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
       <body
         className={`${ubuntu.variable} ${ubuntuMono.variable} ${ubuntu.className} bg-[url('/cozy-city.jpg')] bg-cover bg-fixed antialiased before:absolute before:top-0 before:right-0 before:bottom-0 before:left-0 before:backdrop-blur-xs`}
       >
-        <main className="max-h-screen overflow-auto">
+        <main className="overflow-auto sm:max-h-screen">
           <AboutMe
             name="Zandré Broodryk"
             profilePictureUrl="/avatar.jpg"
